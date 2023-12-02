883fc15dcdde6ae6afae42588373638f2125d577
Vishal Dholariya <vishaldholariya3097@gmail.com>
2023-12-02 18:25:14 -0500

    episode-01

---
+++ App.js
@@ -0,0 +1,15 @@
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"},  [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+]),
+React.createElement("div", {id: "child2"},  [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
